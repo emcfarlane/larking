aba9767b616882ed6b2a34378e2ac2cfe3024bb0
Edward McFarlane <edward.mcfarlane@outlook.com>
2021-10-31 17:06:35 +0000

    Init starlarksql package

--- go.mod
+++ go.mod
@@ -8,20 +8,24 @@ require (
 	github.com/emcfarlane/starlarkrepl v0.0.0-20200525000826-057477e78566
 	github.com/golang/protobuf v1.5.2
 	github.com/google/go-cmp v0.5.6
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/peterh/liner v1.2.1 // indirect
 	github.com/soheilhy/cmux v0.1.5
 	go.starlark.net v0.0.0-20210602144842-1cdb82c9e17a
 	gocloud.dev v0.24.0 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/net v0.0.0-20211020060615-d418f374d309
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/sys v0.0.0-20211029165221-6e7872819dc8 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.5 // indirect
+	golang.org/x/tools v0.1.7 // indirect
 	google.golang.org/genproto v0.0.0-20211019152133-63b7e35f4404
 	google.golang.org/grpc v1.41.0
 	google.golang.org/grpc/examples v0.0.0-20201116223408-707e298f55ef // indirect
 	google.golang.org/protobuf v1.27.1
+	modernc.org/ccgo/v3 v3.12.49 // indirect
+	modernc.org/sqlite v1.13.3 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
--- starlarkthread/starlarkthread.go
+++ starlarkthread/starlarkthread.go
@@ -6,22 +6,55 @@ package starlarkthread
 
 import (
 	"context"
+	"io"
 
 	"go.starlark.net/starlark"
 )
 
-const key = "context"
+const ctxkey = "context"
 
 // SetThreadContext sets the thread context.
 func SetContext(thread *starlark.Thread, ctx context.Context) {
-	thread.SetLocal(key, ctx)
+	thread.SetLocal(ctxkey, ctx)
 }
 
 // Context is a simple helper for getting a threads context.
 // ctx := starlarkthread.Context(thread)
 func Context(thread *starlark.Thread) context.Context {
-	if ctx, ok := thread.Local(key).(context.Context); ok {
+	if ctx, ok := thread.Local(ctxkey).(context.Context); ok {
 		return ctx
 	}
 	return context.Background()
+}
+
+// Resource is a starlark.Value that requires close handling.
+type Resource interface {
+	starlark.Value
+	io.Closer
+}
+
+const rsckey = "resources"
+
+func AddResource(thread *starlark.Thread, rsc Resource) {
+	if rscs, ok := thread.Local(rsckey).(*[]Resource); ok {
+		*rscs = append(*rscs, rsc)
+	}
+	thread.SetLocal(rsckey, &[]Resource{rsc})
+}
+
+func Resources(thread *starlark.Thread) []Resource {
+	if rscs, ok := thread.Local(rsckey).(*[]Resource); ok {
+		return *rscs
+	}
+	return nil
+}
+
+func CloseResources(thread *starlark.Thread) error {
+	rscs := Resources(thread)
+	for _, rsc := range rscs {
+		if err := rsc.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
