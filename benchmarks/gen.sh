f3a92e796727d08949a8b57804fc8e65d67d50ab
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2023-04-19 10:27:23 +0100

    Benchmark envoy proxy with gRPC-JSON transcoding (#80)

--- benchmarks/gen.sh
+++ benchmarks/gen.sh
@@ -2,4 +2,6 @@ protoc -I ~/src/github.com/googleapis/api-common-protos/ -I. \
 	--go_out=module=larking.io/benchmarks:. \
 	--go-grpc_out=module=larking.io/benchmarks:. \
 	--grpc-gateway_out=module=larking.io/benchmarks:. \
+	--include_imports --include_source_info \
+	--descriptor_set_out=api/proto.pb \
 	./api/*.proto
