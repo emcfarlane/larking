e9865f527590898cfdd697f689dbc815a00c7499
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2023-05-12 16:07:29 +0100

    Add compressors interface for Gzip (#101)
    * Add compressors interface for Gzip
    
    * Remove sideaffects on init for encoding register

---
+++ larking/compress.go
@@ -0,0 +1,84 @@
+package larking
+
+import (
+	"compress/gzip"
+	"io"
+	"sync"
+
+	"google.golang.org/grpc/encoding"
+)
+
+// Compressor is used to compress and decompress messages.
+// Based on grpc/encoding.
+type Compressor interface {
+	encoding.Compressor
+}
+
+// CompressorGzip implements the Compressor interface.
+// Based on grpc/encoding/gzip.
+type CompressorGzip struct {
+	Level            *int
+	poolCompressor   sync.Pool
+	poolDecompressor sync.Pool
+}
+
+// Name returns gzip.
+func (*CompressorGzip) Name() string { return "gzip" }
+
+type gzipWriter struct {
+	*gzip.Writer
+	pool *sync.Pool
+}
+
+// Compress implements the Compressor interface.
+func (c *CompressorGzip) Compress(w io.Writer) (io.WriteCloser, error) {
+	z, ok := c.poolCompressor.Get().(*gzipWriter)
+	if !ok {
+		level := gzip.DefaultCompression
+		if c.Level != nil {
+			level = *c.Level
+		}
+		newZ, err := gzip.NewWriterLevel(w, level)
+		if err != nil {
+			return nil, err
+		}
+		return &gzipWriter{Writer: newZ, pool: &c.poolCompressor}, nil
+	}
+	z.Reset(w)
+	return z, nil
+}
+
+func (z *gzipWriter) Close() error {
+	defer z.pool.Put(z)
+	return z.Writer.Close()
+}
+
+type gzipReader struct {
+	*gzip.Reader
+	pool *sync.Pool
+}
+
+// Decompress implements the Compressor interface.
+func (c *CompressorGzip) Decompress(r io.Reader) (io.Reader, error) {
+	z, ok := c.poolDecompressor.Get().(*gzipReader)
+	if !ok {
+		newZ, err := gzip.NewReader(r)
+		if err != nil {
+			return nil, err
+		}
+		return &gzipReader{Reader: newZ, pool: &c.poolDecompressor}, nil
+	}
+	if err := z.Reset(r); err != nil {
+		z.pool.Put(z)
+		return nil, err
+	}
+	return z, nil
+}
+
+func (z *gzipReader) Read(p []byte) (n int, err error) {
+	n, err = z.Reader.Read(p)
+	if err == io.EOF {
+		z.pool.Put(z)
+	}
+	return n, err
+}
